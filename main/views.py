ac54d76a996638ae199c60d7bc3c9244c8110f17
Pradip Kafle <n.pradip101@gmail.com>
2024-02-22 07:20:03 +0545

    fix: add pycache in gitinore

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404, render, redirect
 from .models import BlogpostModel
 from .forms import BlogPostForm
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 @login_required
 def home(request):
@@ -49,8 +51,6 @@ def blogpost_delete(request, pk):
 
 
 
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
     if request.method == 'POST':
